8bc327448252380183ecced1f6cf59da44def2e5
Dragos Nedelcu <dragosgn@gmail.com>
2020-11-16 18:22:16 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -5,37 +5,14 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNew] = useState("");
 
-  const handleChange = (e) => {
-    const newTodo = e.target.value;
-    setNew(newTodo);
-  };
-
-  const handleClick = () => {
-    const newTodos = [...todos, newTodo];
-    if (newTodo && newTodo.length) {
-      setNew("");
-      setTodos(newTodos);
-    }
-  };
-
   return (
     <div>
       <div className="add-todo-box">
-        <input
-          placeholder="Add a todo here..."
-          className="input"
-          onChange={handleChange}
-          type="text"
-          value={newTodo}
-        ></input>
-        <button className="button" onClick={handleClick}>
-          Add todo
-        </button>
+        <input placeholder="Add a todo here..." className="input" type="text" />
+        <button className="button">Add todo</button>
       </div>
       <h3>Todos:</h3>
-      {todos.map((todo) => (
-        <div className="todo">{todo}</div>
-      ))}
+      ... display todos here
     </div>
   );
 }
