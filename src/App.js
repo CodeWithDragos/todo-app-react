e9b1d2df3af66f7bab05c06d98961c13a28d40c1
Dragos Nedelcu <dragosgn@gmail.com>
2020-11-17 10:50:55 +0100

    Added instructions

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  // Create a todos state handler to store all our todos as an array
   const [todos, setTodos] = useState([]);
+  // Create a currenttodo state handler to store the current todo
   const [currentTodo, setNew] = useState("");
+  // Create an error state handler to help us inform the user in case there is an error
+  const [isError, setError] = useState(false);
 
   const handleChange = (e) => {
     // We capture the value of the input and set it as current todo
@@ -19,6 +23,9 @@ function App() {
       setNew("");
       // Set the todos to the new todos array
       setTodos(newTodos);
+    } else if (currentTodo === "") {
+      // Set the error to true to display the error state
+      setError(true);
     }
   };
 
@@ -36,6 +43,7 @@ function App() {
           Add todo
         </button>
       </div>
+      {isError && <p>The todo cannot be empty!</p>}
       <h3>Todos:</h3>
       {todos.map((todo) => (
         <div className="todo">{todo}</div>
